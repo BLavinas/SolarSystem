064c13cd147c6597a162934cc7b05cb31149b03f
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-08-18 17:09:47 -0300

    Requisito 3 corrigido

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropType from 'prop-types';
 
 class Title extends Component {
   render() {
@@ -8,5 +9,9 @@ class Title extends Component {
     );
   }
 }
+
+Title.propTypes = {
+  headline: PropType.string.isRequired,
+};
 
 export default Title;
